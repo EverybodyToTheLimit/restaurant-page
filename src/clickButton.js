8d7962cd43963d8f8b40096fbfa21c5922ae8590
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-12-29 10:25:40 +0000

    add home, menu and contact content

--- src/clickButton.js
+++ src/clickButton.js
@@ -1,3 +1,16 @@
+import { contactSection } from "./contact";
+import {menuSection} from "./menu.js"
+
 export default function btnClicked(someButton) {
+    var sectionTwo = document.getElementById("section-two");
+    if (sectionTwo != null) {
+    sectionTwo.parentNode.removeChild(sectionTwo);
+    }
+    if (someButton == "contact") {
+        contactSection();
+    }
+    else if (someButton == "menu") {
+        menuSection();
+    }
     console.log(someButton);
 }
---
+++ src/menu.js
@@ -0,0 +1,17 @@
+let menuSection = () => {
+    let sectionTwo = document.createElement('div')
+    sectionTwo.id = "section-two";
+    sectionTwo.className = "section-two-contact"
+    let contactDetails = document.createElement('h4')
+    contactDetails.className = "section-two-text"
+    contactDetails.innerHTML = "115 Mayflower Avenue<br>33432 Lexington<br>Utah"
+    let contactMap = document.createElement('img')
+    contactMap.className = "map-image"
+    contactMap.src = "../src/map.PNG"
+    let sectionOne = document.getElementsByClassName('section-one')[0];
+    sectionTwo.appendChild(contactDetails);
+    sectionTwo.appendChild(contactMap);
+    sectionOne.insertAdjacentElement("afterend", sectionTwo);
+}
+
+export {menuSection}
