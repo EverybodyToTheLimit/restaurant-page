4cda15b3b3145b4b4e130dfcf27a8bc24ad30d84
EverybodyToTheLimit <bartoszzawlocki@gmail.com>
2022-12-29 13:16:19 +0000

    add images for webpack dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 
 import './style.css';
 import {buildHeader} from './header-footer';
+import { contactSection } from "./contact";
+import {menuSection} from "./menu.js"
+import {homeSection} from "./home.js"
+
 buildHeader();
